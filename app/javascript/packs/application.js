88bc636448a13dbe1e36f037d42929296159e6ef
rcmll <alice_rc@live.fr>
2020-03-03 17:19:44 +0100

    responsive on contact & h1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,12 +6,12 @@ import {navBar} from '../plugins/navbar.js'
 import {form} from '../plugins/form.js'
 import {egg} from '../plugins/btn.js'
 // import {eggLog} from '../plugins/egg.js'
-import {loader} from '../plugins/loader.js'
+// import {loader} from '../plugins/loader.js'
 
 
 navBar();
 form();
 egg();
 // eggLog();
-loader();
+// loader();
 new WOW().init();
